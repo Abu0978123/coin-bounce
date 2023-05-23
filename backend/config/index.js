360119796fd65e71cd59360c275b444d823dd5b1
Abu Bakar <abubakar0978123@gmail.com>
2023-05-23 10:26:43 -0700

    first commit

---
+++ backend/config/index.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.port;
+const MONGODB_CONNECTION_STRING = process.env.DB_CONNEC;
+
+module.exports = {
+    PORT,
+    MONGODB_CONNECTION_STRING,
+};
