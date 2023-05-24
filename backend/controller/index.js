691d372cc715c85d252c4242cc3ef50766504487
Abu Bakar <abubakar0978123@gmail.com>
2023-05-24 03:06:53 -0700

    error handling

--- backend/controller/index.js
+++ backend/controller/index.js
@@ -1,5 +1,5 @@
 const Joi = require('joi')
-
+const userSchema = require('../models/users');
 const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,25}$/;
 const authController = {
     // user registeration 
@@ -12,11 +12,35 @@ const authController = {
     });
         
         const {error} = userRegisterSchema.validate(req.body);
-        // error handling here
+        // error handling here in validation
         if(error){
            return next(error)
         }
-
+        
+        // if email and user register => return callback
+        const {username, name, email, password} = req.body;
+        const emailInUse = await userSchema.exists({email});
+        const usernameInUse = await userSchema.exists({username});
+        
+        try {
+            if(emailInUse){
+                const error = {
+                    status : 409,
+                    message: "Email already registered, use another email!"
+                }
+                return next(error);
+            }
+            if(usernameInUse){
+                const error = {
+                    status : 409,
+                    message: "username already registered, use another username!"
+                }
+                return next(error);
+            }
+    
+        } catch (error) {
+            return next(error)
+        }
      }, 
     async login() { }, 
 }
